c65fe61e86ceb0c7fb2200b1553d186bfe06ff65
Lyndon White <oxinabox@ucc.asn.au>
2015-05-11 18:43:28 +0800

    =made tools

--- prototypes/tools/cluster_mon/cluster_mon.sh
+++ prototypes/tools/cluster_mon/cluster_mon.sh
@@ -1,6 +1,10 @@
+#!/usr/bin/env bash
+tmux select-layout even-horizontal
+
 tmux split-window -dh 'ssh\ amon'
 tmux split-window -dh 'ssh\ heathred'
 
 tmux send-keys -t 2 'htop' enter
 tmux send-keys -t 3 'htop' enter
 htop
+
--- prototypes/tools/cluster_mon/killworkers.sh
+++ prototypes/tools/cluster_mon/killworkers.sh
@@ -1,4 +1,6 @@
-
+#!/usr/bin/env bash
 pkill worker
 ssh amon pkill worker
+ssh zeus pkill worker
+ssh jove pkill worker
 ssh heathred pkill worker
