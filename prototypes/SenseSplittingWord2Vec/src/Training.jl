b6d24b0308b18d5fc61e11109561166955eb972c
Lyndon White <lyndon.white@research.uwa.edu.au>
2016-05-03 17:00:11 +1000

    =Finally buckled down and moved the softmax classifier into its own module. Didn't hurt as much as expected

--- prototypes/SenseSplittingWord2Vec/src/Training.jl
+++ prototypes/SenseSplittingWord2Vec/src/Training.jl
@@ -1,15 +1,13 @@
 module Training
 
 using Base.Collections
-
+using SoftmaxClassifier
 using WordStreams
 using WordDistributions
 using Trees
 using WordEmbeddings
 using Lumberjack
 using Query
-
-include("Training/softmax_classifier.jl")
 
 
 export train, random_init, run_training!, initialize_embedding
