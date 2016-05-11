8b7fc436e6aff8697e3357d61320cc31189f98a3
Lyndon White <lyndon.white@research.uwa.edu.au>
2016-05-11 15:28:31 +1000

    =Functiong WorkIterator

--- prototypes/SenseSplittingWord2Vec/src/WorkIterator.jl
+++ prototypes/SenseSplittingWord2Vec/src/WorkIterator.jl
@@ -1,9 +1,10 @@
-# Print Hello World
 
-immutable WorkFarmerIterator
-    func :: Function
+"A kind of async parallel iterator map, that does not quiet  presever order."
+immutable WorkFarmerIterator{T}
+    func :: Function #Must take an tuple of const_data, and an element of source_iter and do work on them
     source_iter
-    channel_sizes::Int64
+	const_data::T
+	channel_sizes::Int64
 end
 
 type WorkFarmerState
@@ -14,10 +15,11 @@ end
 
 function Base.start(iter::WorkFarmerIterator)
 
-    o_pending = RemoteChannel(iter.channel_sizes)
-    o_complete = RemoteChannel(iter.channel_sizes)
+    o_pending = RemoteChannel(()->Channel(iter.channel_sizes),myid())
+    o_complete = RemoteChannel(()->Channel(iter.channel_sizes),myid())
 
-    o_job_submitter = remotecall(myid(), o_pending) do pending #Can prob do this without a remotecall
+    o_job_submitter = remotecall(myid(), o_pending, iter.source_iter) do pending, source_iter
+		#Can prob do this without a remotecall
       for wk in source_iter
           put!(pending, wk)
       end
@@ -26,22 +28,55 @@ function Base.start(iter::WorkFarmerIterator)
 
     # Fire off workers that Read Pending (until closed exception)
     # And write to complete
-        remotecall(default_worker_pool()) do
-    end
+	@sync for pid in workers()
+        @async remotecall(pid, iter.func, iter.const_data, o_pending,o_complete) do func, const_data, pending, complete			
+			try
+				while(true)
+					wk = take!(pending) #Block til work arrives
+					res = func(const_data, wk)
+					put!(complete, res) #Block til I can hand work over
+				end			
+			catch ee
+				#Break out of loop when a stream is closed and we try to read pending`
+				#Or in the case of a unfortunte timing error we try to write to complete
+				if !(ee|>typeof ==InvalidStateException && ee.state == :closed)
+					rethrow()
+				end
+				#otherwise eat the InvalidStateException
+			end
+		end
+	end
 
     #Register Finalizer
-
-    WorkFarmerState(o_pending,o_complete, o_job_submitter)
+	state=WorkFarmerState(o_pending,o_complete, o_job_submitter)
+	function finalize_state(st::WorkFarmerState)
+		close(st.pending)
+		close(st.complete)
+	end
+	finalizer(state, finalize_state)
+	state
 end
 
-Base.done()
-#Check that job_submitter is done
-#and complete empty (or closed)
-#and pending empty (or closed)
-#This does leave it with a Potential Glitch that the last few elements might be
-#missed as they are still being worked on, but I don't actually care
+function Base.done(iter::WorkFarmerIterator, state::WorkFarmerState)
+	#Check that job_submitter is done
+	isdone= (isready(state.job_submitter) #Finish submitting
+			&&  !isready(state.complete) #and complete work closed or empty
+			&&  !isready(state.pending)  #and pending empty 
+			)
+	#We can't actually check if the RemoteChannel is closed.
+	if isdone
+		finalize(state) #Done not hurt to finalize it twice
+	end
+	isdone
+end
 
-Base.next()
-#Read complete -- this is a blocking operation so if nothing is ready we'll sit here til it is.
+function Base.next(iter::WorkFarmerIterator, state::WorkFarmerState)
+	#Read complete -- this is a blocking operation so if nothing is ready we'll sit here til it is.
+	res = take!(state.complete)
+	(res,state)
+end
 
-HasLength = HasLength(source)
+Base.iteratorsize(iter::WorkFarmerIterator) = Base.iteratorsize(iter.source_iter)
+
+Base.length(iter::WorkFarmerIterator) = Base.length(iter.source_iter)
+
