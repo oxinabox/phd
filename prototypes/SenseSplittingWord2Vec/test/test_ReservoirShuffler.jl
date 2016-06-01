1cc6d0e27360bdaaaab69bcfe0054cf9c4b3ed8e
Lyndon White <lyndon.white@research.uwa.edu.au>
2016-06-01 13:57:33 +1000

    =

--- prototypes/SenseSplittingWord2Vec/test/test_ReservoirShuffler.jl
+++ prototypes/SenseSplittingWord2Vec/test/test_ReservoirShuffler.jl
@@ -4,12 +4,20 @@ using ReservoirShuffle
 
 
 @testset "Reservoir  Shuffle" begin
-	srand(10)
-	@test ReservoirShuffler(collect(1:1000), 10) ≅ 1:1000
-	@test ReservoirShuffler(collect(1:1000), 1024) ≅ 1:1000
+	srand(15)
+	@test ReservoirShuffler(10:10:100, 64) |> Set == 10:10:100 |> Set
+
+	@test ReservoirShuffler(1:10, 2) |> Set == 1:10 |> Set
+	@test ReservoirShuffler(1:100, 2) |> Set == 1:100 |> Set
+	
+	@test ReservoirShuffler(collect(1:1000), 10) |> Set == Set(1:1000)
+	@test ReservoirShuffler(collect(1:1000), 1024) |> Set == Set(1:1000)
 	@test ReservoirShuffler(collect(1:1000), 1) |> collect == collect(1:1000) # with reservoir of 1 it is exactly the same
 	@test ReservoirShuffler(collect(1:1000), 10) |>collect != collect(1:1000) #Not same order
 	@test ReservoirShuffler(collect(1:1000), 1024) |> collect != collect(1:1000) #not same order
 	
+	@test ReservoirShuffler(1:1_000_000, 1024) |> Set == Set(1:1_000_000)
 
+	@test ReservoirShuffler(1:1_000_000,64) |> Set == Set(1:1_000_000)
+	
 end
