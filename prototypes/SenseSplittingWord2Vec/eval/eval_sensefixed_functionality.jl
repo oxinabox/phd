7d98f242b2578f08c489c931ac071380759a71b4
Lyndon White <lyndon.white@research.uwa.edu.au>
2016-06-16 15:23:04 +1000

    =

--- prototypes/SenseSplittingWord2Vec/eval/eval_sensefixed_functionality.jl
+++ prototypes/SenseSplittingWord2Vec/eval/eval_sensefixed_functionality.jl
@@ -26,9 +26,17 @@ function test_word_embedding()
     
     embed = FixedWordSenseEmbedding(ndims, random_inited, huffman_tree, 
 							  subsampling = 10.0^-5.0,
-							  min_count_for_multiple_senses=20_000, initial_nsenses=20,
+							  min_count_for_multiple_senses=20_000, 
+							  initial_nsenses=20,
 							  min_count=1000, iter=1)
-    @time train(embed, test_file, 
+		 #wikicorp counts: 
+		 #				   20_000 means most common 5000 words, 
+		 #				     8000            ->  mc  10_000 words,
+		 #					 3000			 ->  mc  20_000 words
+		 #				     1600            ->  mc  30_000 words, 
+		 #					  250            ->  mc 100_000 words
+		 #						5			 ->  mc 1.5×10⁶ words
+	@time train(embed, test_file, 
 				end_of_iter_callback=save_callback(model_file),
 	)
 
