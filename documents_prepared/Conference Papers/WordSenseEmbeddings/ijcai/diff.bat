3084965fdb5d80902779c898997bb8237bef132c
Lyndon White <oxinabox@ucc.asn.au>
2017-02-08 12:56:51 +0800

    ijcai made referencess work

---
+++ documents_prepared/Conference Papers/WordSenseEmbeddings/ijcai/diff.bat
@@ -0,0 +1,9 @@
+ latexdiff --exclude-safecmd="section,subsection,subsubsection" --exclude-textcmd="section,subsection,subsubsection" Draft7.tex "AligningWordSenseEmbeddings.tex"  > diff.tex
+ 
+ GOTO LABEL
+ 
+ %\usepackage[subpreambles=true]{standalone}
+ \usepackage{environ}
+ \RenewEnviron{adjustbox}{Nothing here}
+ 
+ :LABEL
