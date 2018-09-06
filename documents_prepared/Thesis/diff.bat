f40ae6d99f588e1a169fae700f0d216cbd1d077d
Lyndon White <oxinabox@ucc.asn.au>
2018-09-06 18:20:50 +0800

    /

--- documents_prepared/Thesis/diff.bat
+++
@@ -1,8 +0,0 @@
- latexdiff -V --math-markup=0 --exclude-safecmd="section,subsection,subsubsection" --exclude-textcmd="section,subsection,subsubsection" draft4.tex "AligningWordSenseEmbeddings.tex"  > diff.tex
- 
- GOTO LABEL
- 
- %\usepackage[subpreambles=true]{standalone}
- \usepackage{environ}
- \RenewEnviron{adjustbox}{[[]]FIGURE OR TABLE EXCLUDED FROM DIFF]]}
- :LABEL
